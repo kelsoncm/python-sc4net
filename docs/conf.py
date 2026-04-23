854d70b74dd46519d184c8b6fdd9420b88131a4e
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-04-23 17:27:36 -0300

    Add initial documentation configuration and module structure

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# docs/conf.py
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "sc4net"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",  # se usar docstring Google/NumPy
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+root_doc = "index"
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "navigation_depth": 4,
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+}
